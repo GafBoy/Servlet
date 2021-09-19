9aceb030efbf63bb25331b464ca8bfba642b1717
gafboy <fear55@list.ru>
2021-09-20 01:27:37 +0500

    Практическое задание №1 Изменить проект таким образом, чтобы обращение ко всем сервлетам было через json, и отвечали они тем же форматом;

--- src/ru/appline/ServletList.java
+++ src/ru/appline/ServletList.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import ru.appline.logic.Model;
 import ru.appline.logic.User;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,55 +22,56 @@ public class ServletList extends HttpServlet {
     Model model = Model.getInstance();
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html; charset=utf-8");
-        PrintWriter pw= response.getWriter();
-int id = Integer.parseInt(request.getParameter("id"));
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html; charset=utf-8");
+//        PrintWriter pw= response.getWriter();
+//int id = Integer.parseInt(request.getParameter("id"));
+//
+//        if (id == 0) {
+//            pw.print("<html>" +
+//                    "<h3>Доступные пользоветали:</h3><br/>" +
+//                    "ID пользователя" +
+//                    "<ul>");
+//            for(Map.Entry<Integer, User> entry : model.getFromList().entrySet()){
+//                pw.print( "<li>" + entry.getKey() + "</li>" +
+//                        "<ul>" +
+//                        "<li>Имя: " + entry.getValue().getName() + "</li>" +
+//                        "<li>Фамилия: " + entry.getValue().getSurname() + "</li>" +
+//                        "<li>Зарплата: " + entry.getValue().getSalary() + "</li>" +
+//                        "</ul>");
+//            }
+//            pw.print("</ul>" +
+//                    "<a href=\"index.jsp\">Домой</a>" +
+//                    "</html>");
+//        }
+//        else if(id > 0) {
+//            if (id>model.getFromList().size()) {
+//                pw.print("<html>" +
+//                        "<h3>Такого пользователя нет :(</h3>" +
+//                        "<a href=\"index.jsp\">Домой</a>" +
+//                        "</html>");
+//            }
+//            else pw.print("<html>" +
+//                    "<h3>Запрошенный пользователь:</h3>" +
+//                    "<br/>" +
+//                    "Имя: " + model.getFromList().get(id).getName() + "<br/>" +
+//                    "Фамилия: " + model.getFromList().get(id).getSurname() + "<br/>" +
+//                    "Зарплата: " + model.getFromList().get(id).getSalary() + "<br/>" +
+//                    "<a href=\"index.jsp\">Домой</a>" +
+//                    "</html>");
+//        }
+//        else
+//            pw.print("<html>" +
+//                    "<h3>ID должен быть больше нуля!</h3>" +
+//                    "<a href=\"index.jsp\">Домой</a>" +
+//                    "</html>");
+//    }
+//}
 
-        if (id == 0) {
-            pw.print("<html>" +
-                    "<h3>Доступные пользоветали:</h3><br/>" +
-                    "ID пользователя" +
-                    "<ul>");
-            for(Map.Entry<Integer, User> entry : model.getFromList().entrySet()){
-                pw.print( "<li>" + entry.getKey() + "</li>" +
-                        "<ul>" +
-                        "<li>Имя: " + entry.getValue().getName() + "</li>" +
-                        "<li>Фамилия: " + entry.getValue().getSurname() + "</li>" +
-                        "<li>Зарплата: " + entry.getValue().getSalary() + "</li>" +
-                        "</ul>");
-            }
-            pw.print("</ul>" +
-                    "<a href=\"index.jsp\">Домой</a>" +
-                    "</html>");
-        }
-        else if(id > 0) {
-            if (id>model.getFromList().size()) {
-                pw.print("<html>" +
-                        "<h3>Такого пользователя нет :(</h3>" +
-                        "<a href=\"index.jsp\">Домой</a>" +
-                        "</html>");
-            }
-            else pw.print("<html>" +
-                    "<h3>Запрошенный пользователь:</h3>" +
-                    "<br/>" +
-                    "Имя: " + model.getFromList().get(id).getName() + "<br/>" +
-                    "Фамилия: " + model.getFromList().get(id).getSurname() + "<br/>" +
-                    "Зарплата: " + model.getFromList().get(id).getSalary() + "<br/>" +
-                    "<a href=\"index.jsp\">Домой</a>" +
-                    "</html>");
-        }
-        else
-            pw.print("<html>" +
-                    "<h3>ID должен быть больше нуля!</h3>" +
-                    "<a href=\"index.jsp\">Домой</a>" +
-                    "</html>");
-    }
-}
-
-protected void doGet (HttpServletResponse response, HttpServletRequest request) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     StringBuffer jb = new StringBuffer();
     String line;
+    int id = Integer.parseInt(request.getParameter("id"));
 
     try {
         BufferedReader reader = request.getReader();
@@ -82,5 +84,21 @@ protected void doGet (HttpServletResponse response, HttpServletRequest request) 
     JsonObject jobj = gson.fromJson(String.valueOf(jb), JsonObject.class);
 
     request.setCharacterEncoding("UTF-8");
-}
+    response.setContentType("application/json; charset = utf-8");
+    PrintWriter pw= response.getWriter();
+
+    if (id == 0) {
+
+    pw.print(gson.toJson(model.getFromList()));
+
+    }
+        else if(id > 0) {
+            if (id>model.getFromList().size()) {
+                pw.print("Такого пользователя нет!");
+            }
+            else pw.print(gson.toJson(model.getFromList().get(id)));
+        }
+        else
+            pw.print("ID должен быть больше нуля!");
+    }
 }
